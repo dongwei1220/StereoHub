30ffb107262a86f8657bc5efc4d3715dd2623e05
Wei Dong <1369852697@qq.com>
2024-08-08 21:49:38 +0800

    Update normalization.py

--- apps/spatial_slice_page/normalization.py
+++ apps/spatial_slice_page/normalization.py
@@ -12,6 +12,18 @@ def normalization():
     return (
         ui.layout_sidebar(
             ui.sidebar(
+                ui.input_slider(
+                    id="nor_target_sum",
+                    label=ui.tags.b("1. Target Sum:"),
+                    min=0,
+                    max=30000,
+                    value=10000,
+                    step=1000,
+                    ticks=True,
+                    animate=True,
+                    width=None,
+                    drag_range=True,
+                ),
                 title=ui.tags.b(icons.setting(), " Parameters"),
             ),
             ui.tags.b(
