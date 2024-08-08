05c74779223a02500a9622f2d34de0d9038b2b53
Wei Dong <1369852697@qq.com>
2024-08-08 21:53:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ app_ui = ui.page_fluid(
             ui.a(
                 "StereoHub ",
                 ui.img(
-                    src="./logo.png",
+                    src="/logo.png",
                     height="36px",
                     width="36px",
                 ),
@@ -186,6 +186,9 @@ def server(input: Inputs, output: Outputs, session: Session):
             'class="dataframe shiny-table table"'
         )
 
+    #############################################
+    ### load data ####
+    #############################################
     def sed_info():
         sed = st.io.read_gef(
             file_path=input.gef_path(),
@@ -231,9 +234,14 @@ def server(input: Inputs, output: Outputs, session: Session):
     stereo_temp = os.path.join(temp_dir, "stereohub")
     os.makedirs(stereo_temp, exist_ok=True)
 
+    #############################################
+    ### qc violin plot ####
+    #############################################
     @render.image
     def qc_violin():
+        ### load data
         sed = sed_info()
+        ### quality control
         sed.tl.cal_qc()
         sed.plt.violin()
         pc = st.plots.PlotCollection(sed)
@@ -282,6 +290,224 @@ def server(input: Inputs, output: Outputs, session: Session):
     @render.download()
     def qc_violin_dl_png():
         path = os.path.join(stereo_temp, "qc_violin.png")
+        return path
+
+    #############################################
+    ### qc filter cell scatter plot #### 
+    #############################################
+    @render.image
+    def qc_filter_scatter():
+        ### load data
+        sed = sed_info()
+        ### quality control
+        sed.tl.cal_qc()
+        ### filter cells
+        sed.tl.filter_cells(
+            min_counts=input.filter_min_counts(),
+            min_genes=input.filter_min_gens(),
+            max_genes=input.filter_max_genes(),
+            pct_counts_mt=input.filter_pct_counts_mt(),
+            filter_raw=True,
+            inplace=True,
+        )
+        sed.plt.genes_count()
+        pc = st.plots.PlotCollection(sed)
+        pc.genes_count(
+            out_path=os.path.join(stereo_temp, "qc_filter_scatter.png"),
+            out_dpi=input.image_dpi2(),
+            x_label=['total_counts', 'total_counts'], 
+            y_label=['pct_counts_mt', 'n_genes_by_counts'], 
+            ncols=2, 
+            dot_size=input.scatter_dot_size(),
+            palette="#808080",
+            width=None, 
+            height=None,
+        )
+        pc.genes_count(
+            out_path=os.path.join(stereo_temp, "qc_filter_scatter.pdf"),
+            out_dpi=input.image_dpi2(),
+            x_label=['total_counts', 'total_counts'], 
+            y_label=['pct_counts_mt', 'n_genes_by_counts'], 
+            ncols=2, 
+            dot_size=input.scatter_dot_size(),
+            palette="#808080",
+            width=None, 
+            height=None,
+        )
+        img: ImgData = {"src": os.path.join(stereo_temp, "qc_filter_scatter.png"), "width": "100%"}
+        return img
+
+    @render.download()
+    def qc_filter_scatter_dl_pdf():
+        path = os.path.join(stereo_temp, "qc_filter_scatter.pdf")
+        return path
+
+    @render.download()
+    def qc_filter_scatter_dl_png():
+        path = os.path.join(stereo_temp, "qc_filter_scatter.png")
+        return path
+    
+    #############################################
+    ### highly variable genes scatter plot #### 
+    #############################################
+    @render.image
+    def hvg_scatter():
+        ### load data
+        sed = sed_info()
+        ### quality control
+        sed.tl.cal_qc()
+        ### filter cells
+        sed.tl.filter_cells(
+            min_counts=input.filter_min_counts(),
+            min_genes=input.filter_min_gens(),
+            max_genes=input.filter_max_genes(),
+            pct_counts_mt=input.filter_pct_counts_mt(),
+            filter_raw=True,
+            inplace=True,
+        )
+        sed.tl.raw_checkpoint()
+        ### data normalization
+        sed.tl.normalize_total(
+            target_sum=input.nor_target_sum(), 
+            inplace=True, 
+            res_key='normalize_total',
+            )
+        sed.tl.log1p(inplace=True, res_key='log1p')
+        ### highly variable genes
+        sed.tl.highly_variable_genes(
+            min_mean=input.hvg_min_mean(),
+            max_mean=input.hvg_max_mean(),
+            min_disp=input.hvg_min_disp(),
+            n_top_genes=input.hvg_n_top_genes(),
+            method=input.hvg_method(),
+            res_key='highly_variable_genes',
+            )
+        sed.plt.highly_variable_genes(res_key='highly_variable_genes')
+        pc = st.plots.PlotCollection(sed)
+        pc.highly_variable_genes(
+            out_path=os.path.join(stereo_temp, "hvg_scatter.png"),
+            out_dpi=input.image_dpi4(),
+            res_key='highly_variable_genes',
+            xy_label=['mean expression of genes', 'dispersions of genes (normalized)'], 
+            xyII_label=['mean expression of genes', 'dispersions of genes (not normalized)'],
+            width=None, 
+            height=None,
+        )
+        pc.highly_variable_genes(
+            out_path=os.path.join(stereo_temp, "hvg_scatter.pdf"),
+            out_dpi=input.image_dpi4(),
+            res_key='highly_variable_genes',
+            xy_label=['mean expression of genes', 'dispersions of genes (normalized)'], 
+            xyII_label=['mean expression of genes', 'dispersions of genes (not normalized)'],
+            width=None, 
+            height=None,
+        )
+        sed.tl.scale()
+        img: ImgData = {"src": os.path.join(stereo_temp, "hvg_scatter.png"), "width": "100%"}
+        return img
+
+    @render.download()
+    def hvg_scatter_dl_pdf():
+        path = os.path.join(stereo_temp, "hvg_scatter.pdf")
+        return path
+
+    @render.download()
+    def hvg_scatter_dl_png():
+        path = os.path.join(stereo_temp, "hvg_scatter.png")
+        return path
+
+    #############################################
+    ### pca elbow plot #### 
+    #############################################
+    @render.image
+    def pca_elbow():
+        ### load data
+        sed = sed_info()
+        ### quality control
+        sed.tl.cal_qc()
+        ### filter cells
+        sed.tl.filter_cells(
+            min_counts=input.filter_min_counts(),
+            min_genes=input.filter_min_gens(),
+            max_genes=input.filter_max_genes(),
+            pct_counts_mt=input.filter_pct_counts_mt(),
+            filter_raw=True,
+            inplace=True,
+        )
+        sed.tl.raw_checkpoint()
+        ### data normalization
+        sed.tl.normalize_total(
+            target_sum=input.nor_target_sum(), 
+            inplace=True, 
+            res_key='normalize_total',
+            )
+        sed.tl.log1p(inplace=True, res_key='log1p')
+        ### highly variable genes
+        sed.tl.highly_variable_genes(
+            min_mean=input.hvg_min_mean(),
+            max_mean=input.hvg_max_mean(),
+            min_disp=input.hvg_min_disp(),
+            n_top_genes=input.hvg_n_top_genes(),
+            method=input.hvg_method(),
+            res_key='highly_variable_genes',
+            )
+        sed.tl.scale()
+        ### pca dimentional reduction
+        sed.tl.pca(
+            use_highly_genes=input.pca_use_highly_genes(),
+            n_pcs=input.pca_ndim(),
+            svd_solver=input.pca_svd_solver(),
+            hvg_res_key='highly_variable_genes',
+            res_key='pca',
+        )
+        sed.plt.elbow(pca_res_key='pca')
+        pc = st.plots.PlotCollection(sed)
+        pc.elbow(
+            out_path=os.path.join(stereo_temp, "pca_elbow_plot.png"),
+            out_dpi=input.image_dpi5(),
+            pca_res_key='pca', 
+            n_pcs=input.pca_ndim(), 
+            title='Elbow Plot', 
+            x_label='Principal Component', 
+            y_label='Variance Explained', 
+            line_width=2, 
+            color='blue', 
+            marker='o', 
+            marker_color=None, 
+            marker_size=4, 
+            cum=False,
+            width=None, 
+            height=None,
+        )
+        pc.elbow(
+            out_path=os.path.join(stereo_temp, "pca_elbow_plot.pdf"),
+            out_dpi=input.image_dpi5(),
+            pca_res_key='pca', 
+            n_pcs=input.pca_ndim(), 
+            title='Elbow Plot', 
+            x_label='Principal Component', 
+            y_label='Variance Explained', 
+            line_width=2, 
+            color='blue', 
+            marker='o', 
+            marker_color=None, 
+            marker_size=4, 
+            cum=False,
+            width=None, 
+            height=None,
+        )
+        
+        img: ImgData = {"src": os.path.join(stereo_temp, "pca_elbow_plot.png"), "width": "100%"}
+        return img
+
+    @render.download()
+    def pca_elbow_dl_pdf():
+        path = os.path.join(stereo_temp, "pca_elbow_plot.pdf")
+        return path
+
+    @render.download()
+    def pca_elbow_dl_png():
+        path = os.path.join(stereo_temp, "pca_elbow_plot.png")
         return path
 
     @render.ui()
